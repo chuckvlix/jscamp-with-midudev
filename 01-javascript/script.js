c33b29dabc8be92b6c49b94ad97e94a236218aaa
Miguel Colque <micky.cm@outlook.es>
2025-12-05 18:40:11 -0400

    Manejo de evento change para filtros y estilado de select

--- 01-javascript/script.js
+++ 01-javascript/script.js
@@ -1,3 +1,21 @@
+// Otros tipos de eventos: 'blur', 'submit', 'change'
+/* const searchInput = document.querySelector('#jobs-search-input')
+
+searchInput?.addEventListener('input', (event) => {
+  const searchTerm = event.target.value.toLowerCase()
+  console.log(searchTerm)
+})
+
+searchInput?.addEventListener('blur', (event) => {
+  console.log('Se dispara cuando el campo pierde el foco')
+})
+
+searchInput?.addEventListener('submit', (event) => {
+  event.preventDefault()
+  console.log('El formulario se ha enviado')
+}) */
+
+// Delegación de eventos
 /* const buttonsApply = document.querySelectorAll('.btn-apply-job')
 
 buttonsApply.forEach(button => {
@@ -7,6 +25,7 @@ buttonsApply.forEach(button => {
   })
 }) */
 
+// Delegación de eventos optima
 const jobListingSection = document.querySelector('.job-listings')
 
 jobListingSection?.addEventListener('click', (event) => {
@@ -17,54 +36,40 @@ jobListingSection?.addEventListener('click', (event) => {
   }
 })
 
+// Recuperar todas las cards de los trabajos
+const jobs = document.querySelectorAll('.job-description-card')
+
+
+// Filtrado de tecnologías
 const filterTechnology = document.querySelector('#filter-technology')
 
-filterTechnology?.addEventListener('change', (event) => {
-  const selectedTech = event.target.value
-  console.log(selectedTech)
+filterTechnology?.addEventListener('change', () => {
+  const selectetTechcnology = filterTechnology.value
 
-  // Mostrar todos los trabajos si no hay una tecnología seleccionada
-  if (!selectedTech) {
-    document.querySelectorAll('.job-description').forEach(job => {
-      job.style.display = 'block'
-    })
-    return
-  }
-
-  // Ocultar los trabajos que no coinciden con la tecnología seleccionada
-  document.querySelectorAll('.job-description').forEach(job => {
-    const jobTechnologies = job.getAttribute('data-technology').split(' ')
-
-    if (jobTechnologies.includes(selectedTech)) {
-      job.style.display = 'block'
+  jobs.forEach(job => {
+    const jobTechnology = job.getAttribute('data-technology')
+    const isShow = selectetTechcnology === '' || jobTechnology === selectetTechcnology
+    job.classList.toggle('is-hidden', !isShow)
+/*     if (selectetTechcnology === '' || jobTechnology === selectetTechcnology) {
+      job.classList.remove('is-hidden') // Mostrar trabajos que coinciden
     } else {
-      job.style.display = 'none'
-    }
+      job.classList.add('is-hidden') // Ocultar trabajos que no coinciden
+    } */
   })
 })
 
+// Filtrado de trabajos por ubicación
 const filterLocation = document.querySelector('#filter-location')
 
-filterLocation?.addEventListener('change', (event) => {
-  const selectLocation = event.target.value
-  console.log(selectLocation)
+filterLocation?.addEventListener('change', () => {
+  const selectLocation = filterLocation.value
 
-  // Mostrar todos los trabajos si no hay una ubicación seleccionada
-  if (!selectLocation) {
-    document.querySelectorAll('.job-description').forEach(job => {
-      job.style.display = 'block'
-    })
-    return
-  }
-
-  // Ocultar los trabajos que no coinciden con la ubicación seleccionada
-  document.querySelectorAll('.job-description').forEach(job => {
+  jobs.forEach(job => {
     const jobLocation = job.getAttribute('data-location')
-
-    if (jobLocation === selectLocation) {
-      job.style.display = 'block'
-    } else {
-      job.style.display = 'none'
-    }
+    const isShow = selectLocation === '' || jobLocation === selectLocation
+    job.classList.toggle('is-hidden', !isShow)
   })
 })
+
+
+
