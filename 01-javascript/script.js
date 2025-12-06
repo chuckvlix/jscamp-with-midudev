ecf164aedecd2169a8281c762f80dc669be65978
Miguel Colque <micky.cm@outlook.es>
2025-12-05 20:49:23 -0400

    JSON y uso de fetch

--- 01-javascript/script.js
+++ 01-javascript/script.js
@@ -36,21 +36,20 @@ jobListingSection?.addEventListener('click', (event) => {
   }
 })
 
-// Recuperar todas las cards de los trabajos
-const jobs = document.querySelectorAll('.job-description-card')
-
 
 // Filtrado de tecnologías
 const filterTechnology = document.querySelector('#filter-technology')
 
 filterTechnology?.addEventListener('change', () => {
-  const selectetTechcnology = filterTechnology.value
+  // Recuperar todas las cards de los trabajos
+  const jobs = document.querySelectorAll('.job-description-card')
+  const selectTechnology = filterTechnology.value
 
   jobs.forEach(job => {
     const jobTechnology = job.getAttribute('data-technology')
-    const isShow = selectetTechcnology === '' || jobTechnology === selectetTechcnology
+    const isShow = selectTechnology === '' || jobTechnology === selectTechnology
     job.classList.toggle('is-hidden', !isShow)
-/*     if (selectetTechcnology === '' || jobTechnology === selectetTechcnology) {
+/*     if (selectTechnology === '' || jobTechnology === selectTechnology) {
       job.classList.remove('is-hidden') // Mostrar trabajos que coinciden
     } else {
       job.classList.add('is-hidden') // Ocultar trabajos que no coinciden
@@ -62,6 +61,7 @@ filterTechnology?.addEventListener('change', () => {
 const filterLocation = document.querySelector('#filter-location')
 
 filterLocation?.addEventListener('change', () => {
+  const jobs = document.querySelectorAll('.job-description-card')
   const selectLocation = filterLocation.value
 
   jobs.forEach(job => {
@@ -72,4 +72,28 @@ filterLocation?.addEventListener('change', () => {
 })
 
 
+// Recuperar datos de un archivo JSON
+fetch('./data.json')
+  .then(response => response.json())
+  .then(jobs => {
+    jobs.forEach(job => {
+      const jobCard = document.createElement('article')
+      jobCard.className = 'job-description-card'
+      jobCard.dataset.technology = job.data.technology
+      jobCard.dataset.location = job.data.location
+      jobCard.dataset.experience = job.data.experience
 
+      jobCard.innerHTML = `
+        <div>
+          <h3>${job.titulo}</h3>
+          <small>${job.empresa} | ${job.ubicacion}</small>
+          <p>${job.descripcion}</p>
+        </div>
+        <button class="btn-apply-job">Aplicar</button>
+      `
+      jobListingSection.appendChild(jobCard)
+    })
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error)
+  })
