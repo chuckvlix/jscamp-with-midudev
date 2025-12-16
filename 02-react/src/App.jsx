385520d4703f91d3609131939c6e525a56546d1d
Miguel Colque <micky.cm@outlook.es>
2025-12-15 21:14:49 -0400

    Componente Route declarativo

--- 02-react/src/App.jsx
+++ 02-react/src/App.jsx
@@ -1,25 +1,15 @@
-import { useRouter } from "./hooks/useRouter.jsx"
 import Navbar from "./components/Navbar.jsx"
 import Footer from "./components/Footer.jsx"
 import HomePage from "./pages/Home.jsx"
 import SearchPage from "./pages/Search.jsx"
-import NotFoundPage from "./pages/NotFoundPage.jsx"
+import { Route } from "./components/Route.jsx"
 
 function App() {
-  const { currentPath } = useRouter()
-
-  let page = <NotFoundPage />
-  if (currentPath === "/") {
-    page = <HomePage />
-  } else if (currentPath === "/search") {
-    page = <SearchPage />
-  }
-
-
   return (
     <>
       <Navbar />
-      {page}
+      <Route path="/" component={HomePage} />
+      <Route path="/search" component={SearchPage} />
       <Footer />
     </>
   )
